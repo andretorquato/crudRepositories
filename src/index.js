84e3f6aef82a2ef0f2080db9545932f18ec5a5fe
andretorquato <ats.torquato1@gmail.com>
2021-05-20 00:35:08 -0300

    feat: tests

---
+++ src/index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+
+const { v4: uuid, v4 } = require("uuid");
+
+const app = express();
+
+app.use(express.json());
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  return response.json(repositories);
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body
+
+  const repository = {
+    id: v4(),
+    title,
+    url,  
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
+});
+
+app.put("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+  const { title, techs, url } = request.body;
+
+  const repository = repositories.find(repository => repository.id === id);
+
+  if (!repository) {
+    return response.status(404).json({ error: "Repository not found" });
+  }
+
+  if(title) repository.title = title;
+  if(techs) repository.techs = techs;
+  if(url) repository.url = url;
+
+
+  return response.json(repository);
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  
+
+  if (repositoryIndex < 0){
+    return response.status(404).json({ error: "Repository not found" });
+  }
+
+  repositories.splice(repositories[repositoryIndex]);
+
+  return response.status(204).send();
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const { id } = request.params;
+
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(404).json({ error: "Repository not found" });
+  }
+
+  repositories[repositoryIndex].likes++;   
+
+  return response.json(repositories[repositoryIndex]);
+});
+
+module.exports = app;
